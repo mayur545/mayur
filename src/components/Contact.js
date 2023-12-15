7ba51f0a8a0ac6ce05e16df803f19835891211de
mayur <skmayur545@gmail.com>
2023-12-15 16:04:59 +0530

    iu

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,65 +2,111 @@ import React from 'react';
 import PlaceIcon from '@mui/icons-material/Place';
 import CallIcon from '@mui/icons-material/Call';
 import EmailIcon from '@mui/icons-material/Email';
+import './contact.css';
+
 
 function Contact() {
   return (
-   <div>
-      <div class="container">
-  <h3><center>CONTACT</center></h3>
-  <p><center>Lets get in touch. Send us a message:</center></p>
-  <div>
+    <div className='back'>
+    
+
+      <div className="container">
+      <h3><center><b>CONTACT</b></center></h3>
+  <p><center><b>Lets get in touch. Send us a message:</b></center></p>
+  <br/>
+
+        <div class="row">
+          <div class="col-sm-6" >
+  
+  <div className='south'>
     <p><i><PlaceIcon /></i> Chicago, US</p>
     <p><i>< CallIcon/></i> Phone: +00 151515</p>
     <p><i><EmailIcon/></i> Email: mail@mail.com</p>
-    <br />
+    </div>
+    </div>
+
    
-   <div>
-    <form>
-      <input type="text" placeholder="Name" required name="Name" /><br/><br/>
-      <input type="text" placeholder="Email" required name="Email" /><br/><br/>
-    <input type="text" placeholder="Subject" required name="Subject" /><br/><br/>
-    <input type="text" placeholder="Message" required name="Message" /><br/><br/>
+   <div class="col-sm-6">
+    <form >
+      <input type="text" placeholder="Name" required name="Name" className='west'/>
+      <input type="text" placeholder="Email" required name="Email"  className='north'/>
+      </form><br/>
+      <form >
+  
+    <input type="text" placeholder="Message" required name="Message" className='east' />
     </form>
-      
-        <button class="w3-button w3-black" type="submit">
-          <i class="fa fa-paper-plane"></i> SEND MESSAGE
-        </button>
+      <br/>
+        <center><button className='moon'><i><EmailIcon/></i>SEND MESSAGE</button></center>
+       
 
       </div>
       </div>
-    <br/>
-    
-    <div class="container">
-    <div class="row">
-      <div class="col-sm-6">
-      <div class="card1">
-      <img src="img_avatar.png"/>
-      <h4><b>John Doe</b></h4> 
-     <p>Architect & Engineer</p>
-     <p><center><button>contact</button></center></p>
-     </div>
-     </div>
+      </div>
 
-     <div class="col-sm-6">
-      <div class="card1">
-      <img src="img_avatar.png"/>
-      <h4><b>John Doe</b></h4> 
-     <p>Architect & Engineer</p>
-     <p><center><button>contact</button></center></p>
-     </div>
-     </div>
+      <br/><br/>
+ 
+      <>
+  {/* Team Section */}
+  <div className="container" >
+    <h3><center><b>THE TEAM</b></center></h3>
+    <p><center>The ones who runs this company</center></p>
+    <div className="row">
+      <div className="col-sm-3">
+        <div className="card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxnoJ4-M8z2EkGS2EO376Y_SbyeKzRDk-ex1VL_PkTyHbGrUJC2sbHHu8VB3Ez06S1fiA&usqp=CAU" className='from' alt=''/>
+          <div className="container">
+          <h4><b>John Doe</b></h4>
+            <h6><b>CEO &amp; Founder</b></h6>
+            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendiss sodales pellentesque elementum. </p>
+              <center><button className='moon'><i><EmailIcon/><b>Contact</b></i></button></center>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3">
+        <div className="card">
+          <img src="https://i.pinimg.com/1200x/d1/0b/8e/d10b8e5d1f0d0dbf64449960c5797d85.jpg"  className='from' alt=''/>
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <h6><b>CEO &amp; Founder</b></h6>
+            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendiss sodales pellentesque elementum. </p>
+            <center><button className='moon'><i><EmailIcon/><b>Contact</b></i></button></center>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3">
+        <div className="card">
+          <img src="https://thumbs.dreamstime.com/b/mascot-animal-domestic-cat-scientist-cute-creative-kawaii-cartoon-logo-252583029.jpg" className='from' alt=''/>
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <h6><b>CEO &amp; Founder</b></h6>
+            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendiss sodales pellentesque elementum. </p>
+            <center><button className='moon'><i><EmailIcon/><b>Contact</b></i></button></center>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3">
+        <div className="card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOiBa7TbZw_w6-foE0Wgap31lzI2uC7fI3mw&usqp=CAU" className='from' alt=''/>
+          <div className="container">
+            <h4><b>John Doe</b></h4>
+            <h6><b>CEO &amp; Founder</b></h6>
+            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendiss sodales pellentesque elementum. </p>
+            <center><button className='moon'><i><EmailIcon/><b>Contact</b></i></button></center>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
      
-  </div>
-  </div>
-
-
+ 
 
 
 
 
  </div>
-  </div>
+
   )
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React from 'react';
+import './services.css';
 
 function Services() {
   return (
-    <div>
-     services 
+   <div className='view'>
+    <div className='container'>
+
+    <div className='row'>
+
+      <div className='col-sm-4'>
+        <div className='card'>
+       
+    <header class="container1">
+      <h1><center><b>Basic</b></center></h1>
+    </header>
+     
+    <div class="container">
+    <p> <b> 10gb</b> Storge</p>
     </div>
+    <hr/>
+    <div class="container">
+   <p><b>10</b>Mails</p>
+    </div>
+    <hr/>
+    
+    <div class="container">
+    <p><b>10</b>Mails</p>
+    </div>
+    <hr/>
+    
+    <div class="container">
+    <p><b>10</b>Mails</p>
+    </div>
+    <hr/>
+    
+    <div class="container">
+    <p><b>10</b>Mails</p>
+    </div>
+     <hr/>
+     
+    <div class="container">
+    <p><b>10</b>Mails</p>
+    </div>
+    
+    
+    <div class="container2">
+    <p><b>10</b>Mails</p>
+    </div>
+  </div>
+          
+          </div> 
+      
+
+      <div className='col-sm-4'>
+      </div>
+
+      <div className='col-sm-4'> 
+      </div>
+
+     
+    </div>
+    
+   </div>
+   </div>
+   
   )
 }
 
