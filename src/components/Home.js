0a242a59761f90af8f8754f6e009ebb6a1c60d70
mayur <skmayur545@gmail.com>
2024-02-20 15:00:34 +0530

    new one

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,21 +8,21 @@ function UncontrolledExample() {
     <div className='topnav'>
     <Carousel>
       <Carousel.Item>
-        <img src='https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=' />
+        <img src='https://images.unsplash.com/photo-1586773860383-dab5f3bc1bcc?q=80&w=1427&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D=' />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src='https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=' />
+      <img src='https://images.unsplash.com/photo-1538108149393-fbbd81895907?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D=' />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src='https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=' />
+      <img src='https://images.unsplash.com/photo-1470116945706-e6bf5d5a53ca?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D=' />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -41,12 +41,12 @@ function UncontrolledExample() {
       <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5RxIIIn7Od_ov6SivkReanM7yo3nlddIBgQ&usqp=CAU'  className='middle'/>
+      <img src='https://images.unsplash.com/photo-1530026405186-ed1f139313f8?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  className='middle'/>
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+      <h1>DR.John Doe</h1> 
+      <p>Heart</p> 
+      <p>A hospital is a healthcare institution providing patient treatment with specialized health science and auxiliary healthcare staff and medical equipment.[1] The best-known type of hospital is the general hospital, which typically has an emergency department to treat</p>
     </div>
   </div>
 </div>
@@ -56,12 +56,12 @@ function UncontrolledExample() {
       <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5RxIIIn7Od_ov6SivkReanM7yo3nlddIBgQ&usqp=CAU'  className='middle'/>
+      <img src='https://images.unsplash.com/photo-1559757175-5700dde675bc?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  className='middle'/>
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+      <h1>DR.John Doe</h1> 
+      <p>Brain</p> 
+      <p>A hospital is a healthcare institution providing patient treatment with specialized health science and auxiliary healthcare staff and medical equipment.[1] The best-known type of hospital is the general hospital, which typically has an emergency department to treat</p>
     </div>
   </div>
 </div>
@@ -71,12 +71,12 @@ function UncontrolledExample() {
       <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5RxIIIn7Od_ov6SivkReanM7yo3nlddIBgQ&usqp=CAU'  className='middle'/>
+      <img src='https://images.unsplash.com/photo-1555708982-8645ec9ce3cc?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  className='middle'/>
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+      <h1>DR.John Doe</h1> 
+      <p>Lungs</p> 
+      <p>A hospital is a healthcare institution providing patient treatment with specialized health science and auxiliary healthcare staff and medical equipment.[1] The best-known type of hospital is the general hospital, which typically has an emergency department to treat</p>
     </div>
   </div>
 </div>
@@ -84,6 +84,8 @@ function UncontrolledExample() {
 
     </div>
     </div>
+
+    
 
     </div>
     
--- src/components/Services.js
+++ src/components/Services.js
@@ -4,65 +4,20 @@ import './services.css';
 function Services() {
   return (
    <div className='view'>
-    <div className='container'>
-
-    <div className='row'>
-
-      <div className='col-sm-4'>
-        <div className='card'>
-       
-    <header class="container1">
-      <h1><center><b>Basic</b></center></h1>
-    </header>
-     
-    <div class="container">
-    <p> <b> 10gb</b> Storge</p>
-    </div>
-    <hr/>
-    <div class="container">
-   <p><b>10</b>Mails</p>
-    </div>
-    <hr/>
-    
-    <div class="container">
-    <p><b>10</b>Mails</p>
-    </div>
-    <hr/>
-    
-    <div class="container">
-    <p><b>10</b>Mails</p>
-    </div>
-    <hr/>
-    
-    <div class="container">
-    <p><b>10</b>Mails</p>
-    </div>
-     <hr/>
-     
-    <div class="container">
-    <p><b>10</b>Mails</p>
-    </div>
-    
-    
-    <div class="container2">
-    <p><b>10</b>Mails</p>
-    </div>
+     <div class="card">
+  <img src="https://www.felixhospital.com/sites/default/files/2022-11/dr-dk-gupta.jpg" alt="John"/>
+  <h1>John Doe</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  {/* <div style="margin: 24px 0;"> */}
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
   </div>
-          
-          </div> 
-      
-
-      <div className='col-sm-4'>
-      </div>
-
-      <div className='col-sm-4'> 
-      </div>
-
-     
-    </div>
-    
-   </div>
-   </div>
+  <p><button>Contact</button></p>
+</div>
+  //  </div>
    
   )
 }
